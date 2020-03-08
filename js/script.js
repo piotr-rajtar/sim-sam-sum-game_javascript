a20d33841d45fa28bae2500b462ab7abf38f2ee2
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-03-08 14:29:25 +0100

    Add player move code in script.js file

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
+//COMPUTER MOVE
+
 let computerMove = 'Nieznany ruch';
 let computerChoice = Math.floor(Math.random() * 3 + 1);
+
+console.log('Komputer wybral: ' + computerChoice);
 
 if (computerChoice == 1) {
   computerMove = 'Kamien';
@@ -10,3 +14,20 @@ if (computerChoice == 1) {
 }
 
 printMessage('Ruch komputera to: ' + computerMove);
+
+//PLAYER MOVE
+
+let playerMove = 'Nieznany ruch';
+let playerInput = prompt('Wybierz swoj ruch: 1.Kamien, 2.Papier, 3.Nozyce');
+
+console.log('Gracz wybral: ' + playerInput);
+
+if (playerInput == 1) {
+  playerMove = 'Kamien';
+} else if (playerInput == 2) {
+  playerMove = 'Papier';
+} else {
+  playerMove ='Nozyce';
+}
+
+printMessage('Ruch gracza to: ' + computerMove);
